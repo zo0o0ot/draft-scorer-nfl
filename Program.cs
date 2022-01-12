785ae0e8798557767be5cde61bf1de405b674108
Ross Larson <zo0o0ot@gmail.com>
2022-01-12 05:36:04 +0000

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using CsvHelper;
+using HtmlAgilityPack;
+using Spectre.Console;
+using System.Text.RegularExpressions;
+using SharpConfig;
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+var scraperConfig = Configuration.LoadFromFile("scraper.conf");
+var pageSection = scraperConfig["Pages"];
+var generalSection = scraperConfig["General"];
