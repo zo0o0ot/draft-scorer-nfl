4b7483760ed56a61b601769809c2b2f24f9c2d33
Ross Larson <zo0o0ot@gmail.com>
2022-01-12 07:23:51 +0000

    adds config file info

--- Program.cs
+++ Program.cs
@@ -5,8 +5,24 @@ using System.Text.RegularExpressions;
 using SharpConfig;
 
 // See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-
-var scraperConfig = Configuration.LoadFromFile("scraper.conf");
-var pageSection = scraperConfig["Pages"];
+//Load config
+var scraperConfig = Configuration.LoadFromFile("scorer.conf");
+var draftSection = scraperConfig["Draft"];
 var generalSection = scraperConfig["General"];
+
+int draftYear = draftSection["DraftYear"].IntValue;
+string urlPattern = draftSection["UrlPattern"].StringValue;
+
+string apikey ="";
+try
+{
+    var localScraperConfig = Configuration.LoadFromFile("scorer.local.conf");
+    var localSection = scraperConfig["General"];
+    apikey = localSection["apikey"].StringValue;
+}
+catch
+{
+    apikey = "";
+}
+
+AnsiConsole.MarkupLine(":abacus: :abacus::abacus::abacus::abacus::abacus:");
