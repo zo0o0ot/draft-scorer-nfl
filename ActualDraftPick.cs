7f8142639cbbe9ab2d555d70d25a25175e5bddab
Ross Larson <zo0o0ot@gmail.com>
2022-01-18 05:06:35 +0000

    reads json to get lists of rounds with picks

---
+++ ActualDraftPick.cs
@@ -0,0 +1,9 @@
+public class ActualDraftPick
+{
+    //Owner,Player,Position,Bid,ProjectedPoints
+    public int Pick { get; set; }
+    public int Round { get; set; }
+    public string Player { get; set; }
+    public string School { get; set; }
+    public bool Traded { get; set; }
+}
--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@ using Spectre.Console;
 using System.Text.RegularExpressions;
 using SharpConfig;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 // See https://aka.ms/new-console-template for more information
 
@@ -50,10 +51,13 @@ using (var csv = new CsvReader(reader, System.Globalization.CultureInfo.Invarian
 bool draftExists =  File.Exists($"actual-draft{Path.DirectorySeparatorChar}{draftYear}{Path.DirectorySeparatorChar}{draftYear}Draft.json");
 // Read in player and school data from CSV file.
 string jsonDraft = "";
-
+dynamic stuff = JsonConvert.DeserializeObject(jsonDraft);
+JObject draft = null;
 if(draftExists && draftComplete)
 {
     AnsiConsole.MarkupLine(":abacus: The draft is complete and we have the draft JSON! No need to load :abacus:");
+    stuff = JsonConvert.DeserializeObject(File.ReadAllText($"actual-draft{Path.DirectorySeparatorChar}{draftYear}{Path.DirectorySeparatorChar}{draftYear}Draft.json"));
+    jsonDraft = File.ReadAllText($"actual-draft{Path.DirectorySeparatorChar}{draftYear}{Path.DirectorySeparatorChar}{draftYear}Draft.json");
 }
 else
 {
@@ -63,16 +67,40 @@ else
         //Send HTTP request from here.
         client.DefaultRequestHeaders.Accept.Clear();
 
-
         var stringTask = client.GetStringAsync("https://api.sportradar.us/draft/nfl/trial/v1/en/2021/draft.json?api_key=nfadymz26xkk28a99c3fedye");
 
         jsonDraft = await stringTask;
         string fileName = $"actual-draft{Path.DirectorySeparatorChar}{draftYear}{Path.DirectorySeparatorChar}{draftYear}Draft.json"; 
         //var Draft = JsonConvert.DeserializeObject<JToken>(jsonDraft);
         File.WriteAllText(fileName, jsonDraft);
-
     }
-
 }
+draft = JObject.Parse(jsonDraft);
+AnsiConsole.MarkupLine(":abacus: Scoring the draft... :abacus:");
+
+//var picks = stuff.rounds.picks;
+string startDate = draft["draft"]["end_date"].ToString();
+List<JArray> draftRounds = new List<JArray>();
+JArray Round1Picks = (JArray)draft["rounds"][0]["picks"];
+JArray Round2Picks = (JArray)draft["rounds"][1]["picks"];
+JArray Round3Picks = (JArray)draft["rounds"][2]["picks"];
+JArray Round4Picks = (JArray)draft["rounds"][3]["picks"];
+JArray Round5Picks = (JArray)draft["rounds"][4]["picks"];
+JArray Round6Picks = (JArray)draft["rounds"][5]["picks"];
+JArray Round7Picks = (JArray)draft["rounds"][6]["picks"];
+
+draftRounds.Add(Round1Picks);
+draftRounds.Add(Round2Picks);
+draftRounds.Add(Round3Picks);
+draftRounds.Add(Round4Picks);
+draftRounds.Add(Round5Picks);
+draftRounds.Add(Round6Picks);
+draftRounds.Add(Round7Picks);
+
+// Now that we have the draft picks, we can score them.
+
+
+dynamic draftasdf = JsonConvert.DeserializeObject(jsonDraft);
+var asdf = "";
 
 
